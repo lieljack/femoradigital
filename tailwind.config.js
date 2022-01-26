c66f6acc798e19c820c173893c5e96ad3f2df259
Eliel Bluejack <bluejackeliel@gmail.com>
2022-01-26 12:06:41 +0100

    Created home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  darkMode: 'class',
+  content: [
+    "./dist/**/*.{html,js}",
+    "./node_modules/@themesberg/flowbite/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#008000",
+        secondary: "#F8A317",
+        "secondary-accent": "#FFC15B",
+        "background-gray": "#1F2028",
+        "background-gray-accent": "#2E3039",
+        "background-gray-3": "#424657",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@themesberg/flowbite/plugin")],
 }
